994d5ea6c162aab93f058c1c9328cb7a84fe9e14
Kochpi <raphael.picoche1@gmail.com>
2026-04-13 17:53:35 +0200

    plus de docu jsdoc

--- routes/catways.js
+++ routes/catways.js
@@ -3,7 +3,12 @@ var router = express.Router();
 const service = require("../Services/catways");
 const auth = require("../middlewares/private");
 
-// Page principale catways
+/**
+ * Affiche la liste des catways (page HTML)
+ * @route GET /page/all
+ * @middleware auth.checkJWT - Vérifie l'authentification JWT
+ * @returns {HTML} Page des catways
+ */
 router.get("/page/all", auth.checkJWT, async (req, res) => {
   try {
     const catways = await service.getAllCatways();
@@ -13,7 +18,13 @@ router.get("/page/all", auth.checkJWT, async (req, res) => {
   }
 });
 
-// Détail d'un catway
+/**
+ * Affiche un catway selon son id
+ * @route GET /page/catways/:id
+ * @middleware auth.checkJWT - Vérifie l'authentification JWT
+ * @param {string} req.params.id - Numéro du catway
+ * @returns {HTML} Détail du catway
+ */
 router.get("/page/:id", auth.checkJWT, async (req, res) => {
   try {
     const catway = await service.getCatwayById(req.params.id);
@@ -23,7 +34,12 @@ router.get("/page/:id", auth.checkJWT, async (req, res) => {
   }
 });
 
-// Créer un catway
+/**
+ * Ajoute un catway
+ * @route ADD /page/catways
+ * @middleware auth.checkJWT - Vérifie l'authentification JWT
+ * @returns {HTML}
+ */
 router.post("/page/add", auth.checkJWT, async (req, res) => {
   try {
     await service.createCatway(req.body);
@@ -34,7 +50,13 @@ router.post("/page/add", auth.checkJWT, async (req, res) => {
   }
 });
 
-// Modifier un catway (seulement catwayState)
+/**
+ * Modifie un catways
+ * @route POST /page/edit/:id
+ * @middleware auth.checkJWT - Vérifie l'authentification JWT
+ * @param {string} req.params.id - Numéro du catway
+ * @returns {HTML}
+ */
 router.post("/page/edit/:id", auth.checkJWT, async (req, res) => {
   try {
     await service.updateCatway(req.params.id, {
@@ -46,7 +68,14 @@ router.post("/page/edit/:id", auth.checkJWT, async (req, res) => {
   }
 });
 
-// Supprimer un catway
+/**
+ * Supprime un catway
+ * @routes DELETE /page/:id
+ * @middleware auth.checkJWT - Vérifie l'authentification JWT
+ * @param {string} req.params.id - Numéro du catway
+ * @returns {HTML}
+ *
+ */
 router.post("/page/delete/:id", auth.checkJWT, async (req, res) => {
   try {
     await service.deleteCatway(req.params.id);
